6fa75bd70c828867785b845a77b9c39b461945cd
nodar23 <makharadze.nodari@gmail.com>
2023-03-28 01:33:48 +0400

    fix: correct code style

--- src/components/Filters/constants.js
+++ src/components/Filters/constants.js
@@ -1,22 +1,22 @@
-const LOW_PRICE = 'LOW_PRICE'
-const HIGH_PRICE = 'HIGH_PRICE'
-const MAX_DISCOUNT = 'MAX_DISCOUNT'
-const MIN_DISCOUNT = 'MIN_DISCOUNT'
-const NEW_DATE = 'NEW_DATE'
-const OLD_DATE = 'OLD_DATE'
+const low_price = 'low_price'
+const high_price = 'high_price'
+const max_discount = 'max_discount'
+const min_discount = 'min_discount'
+const new_date = 'new_date'
+const old_date = 'old_date'
 
 export const PRICE_FILTER = {
-  type: [LOW_PRICE, HIGH_PRICE],
+  type: [low_price, high_price],
   name: 'цена'
 }
 
 export const DISCOUNT_FILTER = {
-  type: [MAX_DISCOUNT, MIN_DISCOUNT],
+  type: [max_discount, min_discount],
   name: 'скидка'
 }
 
 export const DATE_FILTER = {
-  type: [NEW_DATE, OLD_DATE],
+  type: [new_date, old_date],
   name: 'дата'
 }
 
@@ -24,17 +24,17 @@ export const FILTER_QUERY_NAME = 'filterType';
 
 export const getFilteredProducts = ([...products], filterType) => {
   switch (filterType) {
-    case HIGH_PRICE:
+    case high_price:
       return products.sort((a, b) => b.price * ((100 - b.discount) / 100) - a.price * ((100 - a.discount) / 100))
-    case LOW_PRICE:
+    case low_price:
       return products.sort((a, b) => a.price * ((100 - a.discount) / 100) - b.price * ((100 - b.discount) / 100))
-    case MAX_DISCOUNT:
+    case max_discount:
       return products.filter((product) => !!product.discount).sort((a, b) => a.discount - b.discount)
-    case MIN_DISCOUNT:
+    case min_discount:
       return products.filter((product) => !!product.discount).sort((a, b) => b.discount - a.discount)
-    case OLD_DATE:
+    case old_date:
       return products.sort((a, b) => Date.parse(a.created_at) - Date.parse(b.created_at))
-    case NEW_DATE:
+    case new_date:
       return products.sort((a, b) => Date.parse(b.created_at) - Date.parse(a.created_at))
     
     default:
